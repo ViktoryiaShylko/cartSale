d64fdcc3ac538a2718dc06da5d05bcbfaf9bf221
ViktoryiaShylko <vika.shylko@mail.ru>
2025-01-26 19:09:51 +0300

    added colors to js

--- app/script.js
+++ app/script.js
@@ -2,6 +2,7 @@ let prices = document.querySelectorAll('.price');
 let total = document.querySelector('.final-price');
 let sale =  document.querySelector('.sale');
 let btn = document.querySelector('.btn');
+let totalText = document.querySelector('.total');
 
 let sumPrice = 0;
 
@@ -13,5 +14,8 @@ total.innerText = sumPrice;
 
 btn.addEventListener('click', function(){
     let discount = +sale.innerText;
-    total.innerText=sumPrice * (1 - discount / 100);
+    total.innerText  = sumPrice * (1 - discount / 100);
+    totalText.classList.add('red');
+    btn.classList.add('background');
+    btn.innerText = 'Купон на 20% применён';
 })
